2800b13b5f4a73c4e5f0f30d8c36b5091f87f338
FabulousFox <78906517+FabulousCodingFox@users.noreply.github.com>
2023-02-24 23:42:53 +0100

    Update

--- src/main/java/de/fabulousfox/engine/Texture.java
+++ src/main/java/de/fabulousfox/engine/Texture.java
@@ -26,7 +26,7 @@ public class Texture {
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
         // set texture filtering parameters
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
         // load image, create texture and generate mipmaps
